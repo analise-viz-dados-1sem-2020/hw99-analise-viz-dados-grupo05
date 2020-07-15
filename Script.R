1f3f1adc95ec4e123921dfad059ca893468042f9
Raquel Gomes <raquwllmg@gmail.com>
2020-07-15 11:53:41 +0000

    Importação de dados.

--- Script.R
+++ Script.R
@@ -1,4 +1,7 @@
-#Fonte: http://coronavirus.saude.mg.gov.br/images/csv-microdados/csv_sistemas.csv
+library(readr)
+csv_sistemas <- read_delim("http://coronavirus.saude.mg.gov.br/images/csv-microdados/csv_sistemas.csv",
+";", escape_double = FALSE, col_types = cols(DATA_NOTIFICACAO = col_date(format = "%m/%d/%Y")),
+trim_ws = TRUE)
+View(csv_sistemas)
 df<-csv_sistemas
-
-df
+View(df)
