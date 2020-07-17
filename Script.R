5b06df378569b9577db9bdee4b4746f256ac2c0b
Raquel Gomes <raquwllmg@gmail.com>
2020-07-17 12:38:16 +0000

    Atualização banco de dados e inserção das Perguntas.

--- Script.R
+++ Script.R
@@ -1,7 +1,17 @@
 library(readr)
-csv_sistemas <- read_delim("http://coronavirus.saude.mg.gov.br/images/csv-microdados/csv_sistemas.csv",
-";", escape_double = FALSE, col_types = cols(DATA_NOTIFICACAO = col_date(format = "%m/%d/%Y")),
-trim_ws = TRUE)
+
 View(csv_sistemas)
+
 df<-csv_sistemas
-View(df)
+
+df
+
+##Perguntas
+
+#1. Qual o perfil da curva de crescimento (número de casos acumulados) na população acima dos 60 anos?
+
+#2. Existe alguma associação entre comorbidades e a evolução do quadro clínico?
+  
+#3. Existe alguma diferenciação entre os sexos no que se relaciona à evolução do quadro clínico?
+  
+#4. Municípios no Norte e Nordeste possuem piores evoluções dos quadros clínicos em comparação com Belo Horizonte e suas regionais?
